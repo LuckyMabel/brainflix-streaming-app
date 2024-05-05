af3c57c5e25416c2a13e6d5d1b3784b81684002f
Mabel Zhou <zym0306@hotmail.com>
2024-05-04 21:32:46 -0400

    Created the general structure of the next video list.

--- src/components/NextVideo/NextVideo.jsx
+++ src/components/NextVideo/NextVideo.jsx
@@ -1,1 +1,20 @@
 import "./NextVideo.scss";
+
+function NextVideoItem({ video, updateActiveVideo }) {
+  let className = "next-video";
+  const handleClick = () => {
+    updateActiveVideo(video.id);
+  };
+
+  return (
+    <li onClick={handleClick} className={className}>
+      <img className="next-video__image" src={video.image} alt={video.name} />
+      <div className="next-video__info">
+        <div className="next-video__title">{video.title}</div>
+        <div className="next-video__channel">{video.channel}</div>
+      </div>
+    </li>
+  );
+}
+
+export default NextVideoItem;
--- src/components/NextVideosList/NextVideosList.jsx
+++ src/components/NextVideosList/NextVideosList.jsx
@@ -1,10 +1,21 @@
 import "./NextVideosList.scss";
+import NextVideo from "../NextVideo/NextVideo";
 
 function NextVideosList({ videos, updateActiveVideo }) {
   return (
     <section className="next-videos-list">
       <h3 className="next-videos-list__title">NEXT VIDEOS</h3>
-      <ul className="next-videos-list__body"></ul>
+      <ul className="next-videos-list__body">
+        {videos.map((video) => {
+          return (
+            <NextVideo
+              key={video.id}
+              video={video}
+              updateActiveVideo={updateActiveVideo}
+            />
+          );
+        })}
+      </ul>
     </section>
   );
 }
