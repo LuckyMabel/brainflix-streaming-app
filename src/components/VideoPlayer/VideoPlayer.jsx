c0441a0af87bef7facebaaeb6311ac095e384f72
Mabel Zhou <zym0306@hotmail.com>
2024-05-17 01:32:12 -0400

    Set up environment variables

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,9 +1,11 @@
 import "./VideoPlayer.scss";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+
 function VideoPlayer({ videoSrc, image }) {
   return (
     <section className="video-player">
-      <video controls poster={image}>
+      <video controls poster={`${BASE_URL}/${image}`}>
         <source src={videoSrc} type="video/mp4" />
       </video>
     </section>
